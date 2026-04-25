375ca33cca5074011147896682222953f3c9f56f
Sroka, Lukasz (LI - External) <esakis12@gmail.com>
2026-04-25 09:16:15 +0200

    changeDataBase

--- product-list.bat
+++ product-list.bat
@@ -3,67 +3,6 @@ chcp 65001 >nul
 setlocal EnableDelayedExpansion
 cd /d "%~dp0"
 
-echo [product-list] Sprawdzam dostepnosc Docker Desktop...
-docker info >nul 2>&1
-if not errorlevel 1 goto docker_ready
-
-echo [product-list] Docker Desktop nie odpowiada - probuje go uruchomic...
-set "DOCKER_DESKTOP_EXE=%ProgramFiles%\Docker\Docker\Docker Desktop.exe"
-if not exist "%DOCKER_DESKTOP_EXE%" set "DOCKER_DESKTOP_EXE=%ProgramW6432%\Docker\Docker\Docker Desktop.exe"
-if not exist "%DOCKER_DESKTOP_EXE%" set "DOCKER_DESKTOP_EXE=%LocalAppData%\Docker\Docker Desktop.exe"
-if not exist "%DOCKER_DESKTOP_EXE%" (
-    echo [product-list] BLAD: nie znalazlem pliku "Docker Desktop.exe".
-    echo [product-list] Uruchom Docker Desktop recznie i odpal ten skrypt ponownie.
-    pause
-    exit /b 1
-)
-start "" "%DOCKER_DESKTOP_EXE%"
-echo [product-list] Czekam na start silnika Docker Desktop (do 180 s)...
-set "DOCKER_WAIT_RETRIES=90"
-set "DOCKER_WAIT_COUNT=0"
-
-:wait_docker_engine
-timeout /t 2 /nobreak >nul
-docker info >nul 2>&1
-if not errorlevel 1 goto docker_ready
-set /a DOCKER_WAIT_COUNT+=1
-if %DOCKER_WAIT_COUNT% GEQ %DOCKER_WAIT_RETRIES% (
-    echo [product-list] BLAD: Docker Desktop nie wystartowal w oczekiwanym czasie.
-    pause
-    exit /b 1
-)
-goto wait_docker_engine
-
-:docker_ready
-echo [product-list] Docker Desktop gotowy.
-
-echo [product-list] Uruchamiam MSSQL w Dockerze (docker compose up -d)...
-docker compose up -d
-if errorlevel 1 (
-    echo [product-list] BLAD: nie udalo sie uruchomic docker compose.
-    pause
-    exit /b 1
-)
-
-echo [product-list] Czekam na healthcheck kontenera product-list-mssql...
-set "MSSQL_HEALTH_RETRIES=60"
-set "MSSQL_HEALTH_WAITED=0"
-:wait_mssql_health
-for /f "delims=" %%S in ('docker inspect --format "{{.State.Health.Status}}" product-list-mssql 2^>nul') do set "MSSQL_HEALTH_STATUS=%%S"
-if /I "%MSSQL_HEALTH_STATUS%"=="healthy" (
-    echo [product-list] MSSQL healthy.
-    goto mssql_ready
-)
-set /a MSSQL_HEALTH_WAITED+=1
-if %MSSQL_HEALTH_WAITED% GEQ %MSSQL_HEALTH_RETRIES% (
-    echo [product-list] BLAD: MSSQL nie osiagnal stanu healthy w oczekiwanym czasie (status: %MSSQL_HEALTH_STATUS%).
-    pause
-    exit /b 1
-)
-timeout /t 2 /nobreak >nul
-goto wait_mssql_health
-:mssql_ready
-
 echo [product-list] Sprawdzam narzedzie globalne dotnet-ef...
 dotnet tool list -g | findstr /I "dotnet-ef" >nul
 if errorlevel 1 (
